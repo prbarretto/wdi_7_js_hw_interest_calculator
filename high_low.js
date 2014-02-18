31a06c1efe28a4679ba003817eb81982b79f6c2c
Praggya Rustagi Barretto <praggya@gmail.com>
2014-02-18 12:31:42 -0500

    lab

--- high_low.js
+++ high_low.js
@@ -1,1 +1,14 @@
- 
+
+var num = prompt("What's the magic number?");
+
+while(num !== 7) {
+	if (num > 7) {
+		num = prompt("Try again. The magic number is lower.");
+	}
+	else if(num < 7) {
+		num = prompt("Try again. The magic number is higher.");
+	}
+	else {
+		num = alert("You're right, the magic number is 7!")
+	}
+}
--- interest_calc.js
+++ interest_calc.js
@@ -1,1 +1,7 @@
- 
+var principal = prompt("What is your principal amount?");
+var rate = prompt("What is your interest rate?");
+var years = prompt("How many years is your loan?");
+var i = "total interest";
+
+i = principal * rate * years;
+console.log(i);
